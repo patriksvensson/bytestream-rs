72b6e0a50af70e9a12bc3fc9e97673211ba3f9b9
Patrik Svensson <patrik@patriksvensson.se>
2020-06-18 14:03:21 +0200

    Add more tests

--- src/optional.rs
+++ src/optional.rs
@@ -219,7 +219,37 @@ impl<T: Streamable + Eq + Hash, V: Streamable, S: BuildHasher + Default> Streama
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::Cursor;
+    use std::{fmt::Debug, io::Cursor};
+
+    #[derive(Debug, PartialEq)]
+    pub struct Foo {
+        pub foo: u32,
+        pub bar: u16,
+        pub corgi: Vec<u8>,
+        pub waldo: HashMap<i32, String>,
+        pub quux: String,
+    }
+
+    impl Streamable for Foo {
+        fn read_from<R: Read>(buffer: &mut R, order: ByteOrder) -> Result<Self> {
+            Ok(Self {
+                foo: u32::read_from(buffer, order)?,
+                bar: u16::read_from(buffer, order)?,
+                corgi: Vec::<u8>::read_from(buffer, order)?,
+                waldo: HashMap::<i32, String>::read_from(buffer, order)?,
+                quux: String::read_from(buffer, order)?,
+            })
+        }
+
+        fn write_to<W: Write>(&self, buffer: &mut W, order: ByteOrder) -> Result<()> {
+            self.foo.write_to(buffer, order)?;
+            self.bar.write_to(buffer, order)?;
+            self.corgi.write_to(buffer, order)?;
+            self.waldo.write_to(buffer, order)?;
+            self.quux.write_to(buffer, order)?;
+            Ok(())
+        }
+    }
 
     // https://stackoverflow.com/a/27582993/936
     macro_rules! map(
@@ -234,61 +264,22 @@ mod tests {
          };
     );
 
-    #[derive(Debug, PartialEq)]
-    pub struct Foo {
-        pub foo: u32,
-        pub bar: u16,
-        pub baz: Baz,
-        pub corgi: Vec<u8>,
-        pub waldo: HashMap<i32, String>,
-    }
+    fn serialize<T: Streamable>(input: T, order: ByteOrder) -> T {
+        let mut buffer = Vec::<u8>::new();
+        input.write_to(&mut buffer, order).unwrap();
 
-    #[derive(Debug, PartialEq)]
-    pub struct Baz {
-        pub baz: u32,
-    }
-
-    impl Streamable for Foo {
-        fn read_from<R: Read>(buffer: &mut R, order: ByteOrder) -> Result<Self> {
-            Ok(Self {
-                foo: u32::read_from(buffer, order)?,
-                bar: u16::read_from(buffer, order)?,
-                baz: Baz::read_from(buffer, order)?,
-                corgi: Vec::<u8>::read_from(buffer, order)?,
-                waldo: HashMap::<i32, String>::read_from(buffer, order)?,
-            })
-        }
-
-        fn write_to<W: Write>(&self, buffer: &mut W, order: ByteOrder) -> Result<()> {
-            self.foo.write_to(buffer, order)?;
-            self.bar.write_to(buffer, order)?;
-            self.baz.write_to(buffer, order)?;
-            self.corgi.write_to(buffer, order)?;
-            self.waldo.write_to(buffer, order)?;
-            Ok(())
-        }
-    }
-
-    impl Streamable for Baz {
-        fn read_from<R: Read>(buffer: &mut R, order: ByteOrder) -> Result<Self> {
-            Ok(Self {
-                baz: u32::read_from(buffer, order)?,
-            })
-        }
-        fn write_to<W: Write>(&self, buffer: &mut W, order: ByteOrder) -> Result<()> {
-            self.baz.write_to(buffer, order)?;
-            Ok(())
-        }
+        let mut cursor = Cursor::new(buffer);
+        T::read_from(&mut cursor, order).unwrap()
     }
 
     #[test]
-    pub fn should_serialize_custom_struct() {
+    pub fn custom_struct() {
         let foo = Foo {
             foo: 31,
             bar: 7,
-            baz: Baz { baz: 23 },
             corgi: vec![1, 2, 3, 4],
             waldo: map! { 1 => "A".to_owned(), 2 => "B".to_owned() },
+            quux: "foo".to_owned(),
         };
 
         let mut buffer = Vec::<u8>::new();
@@ -298,5 +289,110 @@ mod tests {
         let result = Foo::read_from(&mut cursor, ByteOrder::BigEndian).unwrap();
 
         assert_eq!(foo, result);
+    }
+
+    mod std_types {
+        use super::*;
+        use test_case::test_case;
+
+        #[test_case(u8::max_value(), ByteOrder::BigEndian ; "u8 big endian")]
+        #[test_case(u8::max_value(), ByteOrder::LittleEndian ; "u8 little endian")]
+        #[test_case(u16::max_value(), ByteOrder::BigEndian ; "u16 big endian")]
+        #[test_case(u16::max_value(), ByteOrder::LittleEndian ; "u16 little endian")]
+        #[test_case(u32::max_value(), ByteOrder::BigEndian ; "u32 big endian")]
+        #[test_case(u32::max_value(), ByteOrder::LittleEndian ; "u32 little endian")]
+        #[test_case(u64::max_value(), ByteOrder::BigEndian ; "u64 big endian")]
+        #[test_case(u64::max_value(), ByteOrder::LittleEndian ; "u64 little endian")]
+        #[test_case(i8::max_value(), ByteOrder::BigEndian ; "i8 big endian")]
+        #[test_case(i8::max_value(), ByteOrder::LittleEndian ; "i8 little endian")]
+        #[test_case(i16::max_value(), ByteOrder::BigEndian ; "i16 big endian")]
+        #[test_case(i16::max_value(), ByteOrder::LittleEndian ; "i16 little endian")]
+        #[test_case(i32::max_value(), ByteOrder::BigEndian ; "i32 big endian")]
+        #[test_case(i32::max_value(), ByteOrder::LittleEndian ; "i32 little endian")]
+        #[test_case(i64::max_value(), ByteOrder::BigEndian ; "i64 big endian")]
+        #[test_case(i64::max_value(), ByteOrder::LittleEndian ; "i64 little endian")]
+        pub fn max_value<T: Streamable + PartialEq + Debug + Copy + Clone>(
+            input: T,
+            order: ByteOrder,
+        ) {
+            // Given, When
+            let result = serialize(input, order);
+            // Then
+            assert_eq!(input, result);
+        }
+
+        #[test_case(u8::min_value(), ByteOrder::BigEndian ; "u8 big endian")]
+        #[test_case(u8::min_value(), ByteOrder::LittleEndian ; "u8 little endian")]
+        #[test_case(u16::min_value(), ByteOrder::BigEndian ; "u16 big endian")]
+        #[test_case(u16::min_value(), ByteOrder::LittleEndian ; "u16 little endian")]
+        #[test_case(u32::min_value(), ByteOrder::BigEndian ; "u32 big endian")]
+        #[test_case(u32::min_value(), ByteOrder::LittleEndian ; "u32 little endian")]
+        #[test_case(u64::min_value(), ByteOrder::BigEndian ; "u64 big endian")]
+        #[test_case(u64::min_value(), ByteOrder::LittleEndian ; "u64 little endian")]
+        #[test_case(i8::min_value(), ByteOrder::BigEndian ; "i8 big endian")]
+        #[test_case(i8::min_value(), ByteOrder::LittleEndian ; "i8 little endian")]
+        #[test_case(i16::min_value(), ByteOrder::BigEndian ; "i16 big endian")]
+        #[test_case(i16::min_value(), ByteOrder::LittleEndian ; "i16 little endian")]
+        #[test_case(i32::min_value(), ByteOrder::BigEndian ; "i32 big endian")]
+        #[test_case(i32::min_value(), ByteOrder::LittleEndian ; "i32 little endian")]
+        #[test_case(i64::min_value(), ByteOrder::BigEndian ; "i64 big endian")]
+        #[test_case(i64::min_value(), ByteOrder::LittleEndian ; "i64 little endian")]
+        pub fn min_value<T: Streamable + PartialEq + Debug + Copy + Clone>(
+            input: T,
+            order: ByteOrder,
+        ) {
+            // Given, When
+            let result = serialize(input, order);
+            // Then
+            assert_eq!(input, result);
+        }
+
+        // Endianess shouldn't matter for booleans, since it's only a byte,
+        // but we still want a test for it.
+        #[test_case(true, ByteOrder::BigEndian ; "true big endian")]
+        #[test_case(true, ByteOrder::LittleEndian ; "true little endian")]
+        #[test_case(false, ByteOrder::BigEndian ; "false big endian")]
+        #[test_case(false, ByteOrder::LittleEndian ; "false little endian")]
+        pub fn boolean(input: bool, order: ByteOrder) {
+            // Given, When
+            let result = serialize(input, order);
+            // Then
+            assert_eq!(input, result);
+        }
+
+        #[test_case("Hello World".to_owned(), ByteOrder::BigEndian ; "big endian")]
+        #[test_case("Hello World".to_owned(), ByteOrder::LittleEndian ; "little endian")]
+        pub fn string(input: String, order: ByteOrder) {
+            // Given, When
+            let result = serialize(input.clone(), order);
+            // Then
+            assert_eq!(input, result);
+        }
+
+        #[test_case(vec![0u8, 1u8, 2u8, 3u8, 4u8], ByteOrder::BigEndian ; "u8 big endian")]
+        #[test_case(vec![0u8, 1u8, 2u8, 3u8, 4u8], ByteOrder::LittleEndian ; "u8 little endian")]
+        #[test_case(vec!["A".to_owned(), "B".to_owned()], ByteOrder::BigEndian ; "String big endian")]
+        #[test_case(vec!["A".to_owned(), "B".to_owned()], ByteOrder::LittleEndian ; "String little endian")]
+        pub fn vec<T: Streamable + PartialEq + Debug + Clone>(input: Vec<T>, order: ByteOrder) {
+            // Given, When
+            let result = serialize(input.clone(), order);
+            // Then
+            assert_eq!(input, result);
+        }
+
+        #[test_case(ByteOrder::BigEndian ; "big endian")]
+        #[test_case(ByteOrder::LittleEndian ; "little endian")]
+        pub fn map(order: ByteOrder) {
+            // Given
+            let input = map! {
+                1 => "A".to_owned(),
+                2 => "B".to_owned(),
+                3 => "C".to_owned()
+            };
+            // When
+            let result = serialize(input.clone(), order);
+            // Then
+            assert_eq!(input, result);
+        }
     }
 }
